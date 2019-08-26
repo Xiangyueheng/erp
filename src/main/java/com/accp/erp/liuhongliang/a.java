0cd2fe197b0e0547b75f230589bab5c042153143
xyh <2628846043@qq.com>
2019-08-26 10:00:17 +0800

    追加a.class 保证git读到我的文件夹
    追加a.class 保证git读到我的文件夹

---
+++ src/main/java/com/accp/erp/liuhongliang/a.java
@@ -0,0 +1,8 @@
+package com.accp.erp.liuhongliang;
+
+/**
+ * @author xiangyueheng
+ * @create 2019-08-26 9:57
+ */
+public class a {
+}
---
+++ src/main/java/com/accp/erp/xiangyueheng/a.java
@@ -0,0 +1,8 @@
+package com.accp.erp.xiangyueheng;
+
+/**
+ * @author xiangyueheng
+ * @create 2019-08-26 9:57
+ */
+public class a {
+}
